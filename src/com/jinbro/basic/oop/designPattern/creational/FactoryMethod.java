da01190049028a8fb935ea1132271a2b2e7d3d2d
imjinbro <jinbro@likelion.org>
2018-01-02 13:26:08 +0900

    객체지향 이렇게 썼다(디자인패턴) : 팩토리메서드 연습문제(스테이트 + 템플릿 + 싱글턴도 같이 사용) 코드 업데이트(해당 부분 주석 써둠)

--- src/com/jinbro/basic/oop/designPattern/creational/FactoryMethod.java
+++ src/com/jinbro/basic/oop/designPattern/creational/FactoryMethod.java
@@ -1,8 +1,6 @@
 package com.jinbro.basic.oop.designPattern.creational;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /*
     [팩토리 메서드 패턴]
@@ -170,7 +168,366 @@ class ElevatorManager{
     }
 }
 
+
+/****************** 연습문제 : 스테이트 + 템플릿메서드 + 팩토리메서드(차 상태 인스턴스를 관리 : 다양할 수 있기때문, 상태 인스턴스는 싱글턴 - 동기화처리) ******************/
+class Car{
+    private int speed = 0;
+    private CarState carState;
+    private CarStateFactory factory = CarStateFactory.getFactory();
+
+    public Car() {
+        this.carState = factory.getCarState(this, CarStateID.NORMAL);
+    }
+
+    public void setCarState(CarStateID carStateID) {
+        //인자로 넘겨줄 때 직접 생성하지않고 enum을 던져주면 알아서 CarStateFactory에서 return
+        carState = factory.getCarState(this, carStateID);
+    }
+
+    //각 상태에 따라 스피드업, 다운이 정해짐
+    public void speedUp(int speed){
+        carState.speedUp(speed);
+    }
+
+    public void speedDown(int speed){
+        carState.speedDown(speed);
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+}
+
+enum CarStateID {
+    NORMAL,
+    LIMP
+}
+
+class CarStateFactory{
+    private static CarStateFactory factory = new CarStateFactory();
+    private Set<CarState> stateSet;
+    private CarStateFactory(){
+        stateSet = new HashSet<>();
+    }
+
+    public static CarStateFactory getFactory() {
+        return factory;
+    }
+
+    private boolean isExist(CarState state){
+        return stateSet.contains(state);
+    }
+
+    public void addCarSate(CarState state){
+        if(isExist(state)){
+            return;
+        }
+
+        stateSet.add(state);
+    }
+
+    public void removeCarState(CarState state){
+        if(!isExist(state)){
+            return;
+        }
+
+        stateSet.remove(state);
+    }
+
+    /* 각각 인스턴스를 얻어오는 과정이 다를 수 있어서 팩토리를 사용함 */
+    public CarState getCarState(Car car, CarStateID stateID){
+        CarState state = null;
+
+        switch(stateID){
+            case NORMAL:
+                state = NormalCarState.getState(car);
+                break;
+
+            case LIMP:
+                state = LimpCarState.getState(car);
+                break;
+        }
+
+        return state;
+    }
+}
+
+abstract class CarState{
+    protected Car car;
+
+    public CarState(Car car) {
+        this.car = car;
+    }
+
+    private boolean isNegativeSpeed(int speed){
+        return speed < 0;
+    }
+
+    public abstract void stateSpeedUp(int speed);
+    public void speedUp(int speed){
+        if(isNegativeSpeed(speed)){
+            return;
+        }
+        stateSpeedDown(speed);
+    }
+
+    public abstract void stateSpeedDown(int speed);
+    public void speedDown(int speed){
+        if(isNegativeSpeed(speed)){
+            return;
+        }
+        stateSpeedDown(speed);
+    }
+}
+
+class NormalCarState extends CarState{
+    private static NormalCarState state;
+    private NormalCarState(Car car) {
+        super(car);
+    }
+
+    public synchronized static NormalCarState getState(Car car){
+        if(state == null){
+            new NormalCarState(car);
+        }
+
+        return state;
+    }
+
+    @Override
+    public synchronized void stateSpeedUp(int speed) {
+        car.setSpeed(speed);
+    }
+
+    @Override
+    public synchronized void stateSpeedDown(int speed) {
+        car.setSpeed(speed);
+    }
+
+
+}
+
+class LimpCarState extends CarState{
+    private static LimpCarState state;
+    private static final int LIMIT_SPEED = 60;
+    private LimpCarState(Car car) {
+        super(car);
+    }
+
+    public synchronized static LimpCarState getState(Car car){
+        if(state == null){
+            state = new LimpCarState(car);
+        }
+
+        return state;
+    }
+
+    private synchronized boolean isLimitSpeed(int speed){
+        return speed > LIMIT_SPEED;
+    }
+
+    @Override
+    public synchronized void stateSpeedUp(int speed) {
+        if(isLimitSpeed(speed)){
+            car.setSpeed(LIMIT_SPEED);
+            return;
+        }
+
+        car.setSpeed(speed);
+    }
+
+    @Override
+    public synchronized void stateSpeedDown(int speed) {
+        car.setSpeed(speed);
+    }
+
+}
+
+
+
+/*************** 연습문제 : 팩토리메서드 패턴 + 템플릿메서드 패턴 : 생성관리자 + 중복되는 부분과 각각의 부분 처리 ***************/
 enum Direction{
     UP,
     DOWN
+}
+
+
+enum MotorStatus{
+    MOVING,
+    STOPPED
+}
+
+enum MotorMaker{
+    HYUNDAI,
+    LG
+}
+
+class MotorFactory{
+    private Map<MotorMaker, Motor> motorMap = new HashMap<>();
+
+    private boolean isExist(MotorMaker maker){
+        return motorMap.containsKey(maker);
+    }
+
+    public void addMotor(MotorMaker maker, Motor motor){ //생성해두고 관리하거나 생성방식 여러가지를 여기서 정의해두거나
+        if(isExist(maker)){
+            return;
+        }
+
+        /*switch (maker){
+            case HYUNDAI:
+                //각각의 생성방식
+                break;
+
+            case LG:
+                //각각의 생성방식
+                break;
+        }*/
+        motorMap.put(maker, motor);
+    }
+
+    public void removeMotor(MotorMaker maker){
+        if(!isExist(maker)){
+            throw new NullPointerException("리스트에 없음");
+        }
+        motorMap.remove(maker);
+    }
+
+    public Motor getMotor(MotorMaker maker){
+        if(!isExist(maker)){
+            throw new NullPointerException("리스트에 없음");
+        }
+        return motorMap.get(maker);
+    }
+}
+
+abstract class Motor{
+    private MotorStatus motorStatus;
+
+    public Motor() {
+        this.motorStatus = MotorStatus.STOPPED;
+    }
+
+    public MotorStatus getMotorStatus() {
+        return motorStatus;
+    }
+
+    private void setMotorStatus(MotorStatus motorStatus) {
+        this.motorStatus = motorStatus;
+    }
+
+    private boolean isStopped(){
+        return motorStatus.equals(MotorStatus.STOPPED);
+    }
+
+    public void move(Direction direction){
+        if(!isStopped()){
+            return;
+        }
+
+        setMotorStatus(MotorStatus.MOVING);
+        moveMotor(direction);
+
+    }
+
+    protected abstract void moveMotor(Direction direction);
+
+
+    public void stop(){
+        if(isStopped()){
+            return;
+        }
+        setMotorStatus(MotorStatus.STOPPED);
+    }
+}
+
+class LGMotor extends Motor{
+    private static LGMotor motor = new LGMotor();
+    private LGMotor(){}
+
+    public static LGMotor getMotor() {
+        return motor;
+    }
+
+    @Override
+    protected void moveMotor(Direction direction) {
+        System.out.println("LG Motor : " + direction);
+    }
+}
+
+class HyundaiMotor extends Motor{
+    private static HyundaiMotor motor = new HyundaiMotor();
+    private HyundaiMotor(){}
+
+    public static HyundaiMotor getMotor(){
+        return motor;
+    }
+
+    @Override
+    protected void moveMotor(Direction direction) {
+        System.out.println("Hyundai Motor : " + direction);
+    }
+}
+
+class Elavator{
+    private int id;
+    private int floor = 1; //default
+    private Motor motor;
+    private ElevatorPrinter printer;
+
+    public Elavator(int id, Motor motor, ElevatorPrinter printer) {
+        this.id = id;
+        this.motor = motor;
+        this.printer = printer;
+    }
+
+    private boolean isInValidFloor(int floor){
+        return getFloor() == floor;
+    }
+
+    private Direction getDirection(int floor){
+        if(getFloor() > floor){
+            return Direction.DOWN;
+        }
+
+        return Direction.UP;
+    }
+
+    public void gotoFloor(int floor){
+        if(isInValidFloor(floor)){
+            return;
+        }
+
+        motor.moveMotor(getDirection(floor));
+        setFloor(floor);
+        motor.stop();
+        printer.print("[" + id + "] : " + getFloor() + "층으로 이동됨");
+    }
+
+    public int getFloor() {
+        return floor;
+    }
+
+    private void setFloor(int floor) {
+        this.floor = floor;
+    }
+
+    public void setPrinter(ElevatorPrinter printer) {
+        this.printer = printer;
+    }
+}
+
+interface ElevatorPrinter{
+    void print(String message);
+}
+
+class ConsoleElevatorPrinter implements ElevatorPrinter{
+    @Override
+    public void print(String message) {
+        System.out.println(message);
+    }
 }
